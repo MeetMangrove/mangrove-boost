21f64415ce7d6c1352f63e7842e8f90d366b2a47
Steven Sanséau <stevensanseau@Stevens-MacBook-Pro.local>
2017-03-20 18:06:26 +0100

    first commit

---
+++ controllers/api.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const bluebird = require('bluebird');
+const request = bluebird.promisifyAll(require('request'), { multiArgs: true });
+const graph = require('fbgraph');
+const Twit = require('twit');
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+exports.getApi = (req, res) => {
+  res.render('api/index', {
+    title: 'API Examples'
+  });
+};
+
+
+/**
+ * GET /api/facebook
+ * Facebook API example.
+ */
+exports.getFacebook = (req, res, next) => {
+  const token = req.user.tokens.find(token => token.kind === 'facebook');
+  graph.setAccessToken(token.accessToken);
+  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err, results) => {
+    if (err) { return next(err); }
+    res.render('api/facebook', {
+      title: 'Facebook API',
+      profile: results
+    });
+  });
+};
+
+
+/**
+ * POST /api/twitter
+ * Post a tweet.
+ */
+exports.postTwitter = (req, res, next) => {
+  req.assert('tweet', 'Tweet cannot be empty').notEmpty();
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/api/twitter');
+  }
+
+  const token = req.user.tokens.find(token => token.kind === 'twitter');
+  const T = new Twit({
+    consumer_key: process.env.TWITTER_KEY,
+    consumer_secret: process.env.TWITTER_SECRET,
+    access_token: token.accessToken,
+    access_token_secret: token.tokenSecret
+  });
+
+  T.post('statuses/update', { status: req.body.tweet }, (err) => {
+    if (err) { return next(err); }
+    req.flash('success', { msg: 'Your tweet has been posted.' });
+    res.redirect('/api/twitter');
+  });
+};
+
+
+/**
+ * GET /api/upload
+ * File Upload API example.
+ */
+
+exports.getFileUpload = (req, res) => {
+  res.render('api/upload', {
+    title: 'File Upload'
+  });
+};
+
+exports.postFileUpload = (req, res) => {
+  req.flash('success', { msg: 'File was uploaded successfully.' });
+  res.redirect('/api/upload');
+};
---
+++ controllers/onboarding.js
@@ -0,0 +1,34 @@
+/**
+ * GET /onboarding/step1
+ * Slack register.
+ */
+exports.step1 = (req, res) => {
+  if(req.user && req.user.slack){
+    return res.redirect('/onboarding/step2');
+  }
+
+  res.render('onboarding/step1', {
+    title: 'Create account'
+  });
+};
+
+/**
+ * GET /onboarding/step2
+ * Link social Network account.
+ */
+exports.step2 = (req, res) => {
+  res.render('onboarding/step2', {
+    title: 'Link Social Network Account'
+  });
+};
+
+
+/**
+ * GET /onboarding/step3
+ * Thank user
+ */
+exports.step3 = (req, res) => {
+  res.render('onboarding/step3', {
+    title: 'Keep in touch !'
+  });
+};
