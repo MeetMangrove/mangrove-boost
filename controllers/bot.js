cd22454285981acae4a9cfb0dacfb4a33f3f4d7e
Antonin Archer <antoarcher@gmail.com>
2017-03-25 15:40:52 +0100

    Set up bot instance that responds to any message

--- controllers/bot.js
+++ controllers/bot.js
@@ -1,13 +1,42 @@
-var Botkit = require('botkit');
+const Botkit = require('botkit');
+const dotenv = require('dotenv');
+
+dotenv.load({ path: '.env' });
 
 
-var controller = Botkit.slackbot({
-    debug: true,
+const controller = Botkit.slackbot({
+  debug: true,
 });
 
-var bot = controller.spawn({
-    token: process.env.token,
-    incoming_webhook: {
-        url: ""
-    }
+const bot = controller.spawn({
+  token: process.env.SLACK_BOT_TOKEN,
+  incoming_webhook: {
+    url: 'https://hooks.slack.com/services/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'
+  }
 }).startRTM();
+
+controller.hears('', 'direct_mention', function(bot, message) {
+  bot.reply(message, {
+    attachments:[
+      {
+        title: 'Do you want to interact with my buttons?',
+        callback_id: '123',
+        attachment_type: 'default',
+        actions: [
+          {
+            "name":"yes",
+            "text": "Yes",
+            "value": "yes",
+            "type": "button",
+          },
+          {
+            "name":"no",
+            "text": "No",
+            "value": "no",
+            "type": "button",
+          }
+        ]
+      }
+    ]
+  });
+});
